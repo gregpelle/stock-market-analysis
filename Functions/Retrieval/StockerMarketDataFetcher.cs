15490a912d90156a259528866b14a2c0274dcd53
Greg Pelle <6699402+gregpelle@users.noreply.github.com>
2021-02-13 09:57:26 -0500

    Added stub for cosmodb input binding

--- Functions/Retrieval/StockerMarketDataFetcher.cs
+++ Functions/Retrieval/StockerMarketDataFetcher.cs
@@ -8,7 +8,14 @@ namespace gregpelle.Retrieval
     public static class StockerMarketDataFetcher
     {
         [FunctionName("StockerMarketDataFetcher")]
-        public static void Run([TimerTrigger("0 0 20 * * Mon-Fri")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 0 20 * * Mon-Fri")]TimerInfo myTimer,
+        [CosmosDB(
+                databaseName: "ToDoItems",
+                collectionName: "Items",
+                ConnectionStringSetting = "CosmosDBConnection",
+                Id = "{ToDoItemId}",
+                PartitionKey = "{ToDoItemPartitionKeyValue}")]ToDoItem toDoItem,
+         ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
